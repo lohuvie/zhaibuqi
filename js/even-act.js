b7a565103951cbf34cea121122cdb2c5cb7e6f08
lohuvie <lohuvie@gmail.com>
2013-03-18 12:55:25 +0800

    登陆判定 新建活动修改 我要喜欢参与点击

--- js/even-act.js
+++ js/even-act.js
@@ -5,6 +5,21 @@ $(document).ready(function() {
     var join_num = 0;
     var is_love = -1;
     var is_join = -1;
+
+    var loveBtn = $('#love');    //love按钮
+    var joinBtn = $('#join');    //join按钮
+    var loveNum = $('#love-num');    //love数
+    var joinNum = $('#join-num');    //join数
+
+    var loveSpan = $("#love span");
+    var joinSpan = $("#love span");
+
+    var $alreadylove = $("<span>已喜欢!</span>");
+    var $wantlove = $("<span>我也喜欢</span>")
+    var $alreadyjoin = $("<span>已参与!</span>");
+    var $wantjoin = $("<span>我要参与</span>")
+
+    //加载的初始判断
     $.ajax({ 
         type:"GET", 
         url:"json/love_activity_count.php?activity="+id,
@@ -15,48 +30,39 @@ $(document).ready(function() {
             join_num = data.join_count;
             is_love = data.is_love;
             is_join = data.is_join;
-            $('love-num').empty();
-            $('#love-num').append(like_num);
-            $('join-num').empty();
-            $('#join-num').append(join_num);
+            loveNum.empty();
+            loveNum.append(like_num);
+            joinNum.empty();
+            joinNum.append(join_num);
 
-            
             //判定是否喜欢了活动
-            if(is_love == 0)
-                $('#love').attr('class','collect-btn');
-            else if(is_love == 1)
-                $('#love').attr('class','');
-            else
+            if(is_love === 0){
+                loveBtn.attr('class','collect-btn');
+                loveBtn.html($wantlove);
+            }else if(is_love === 1){
+                loveBtn.attr('class','already-btn');
+                loveBtn.html($alreadylove);
+            }else{
                 console.log("服务器错误,请重新加载页面");
-            
+            }
+
             //判定是否参与了活动
-            if(is_join == 0)
-                $('#join').attr('class','collect-btn');
-            else if(is_join == 1)
-                $('#join').attr('class','');
-            else
+            if(is_join === 0){
+                joinBtn.attr('class','collect-btn');
+                joinBtn.html($wantjoin);
+            }
+            else if(is_join === 1){
+                joinBtn.attr('class','already-btn');
+                joinBtn.html($alreadyjoin);
+            }
+            else{
                 console.log("服务器错误,请重新加载页面");
-            
+            }
         } 
     }); 
-        /*
-        var love = $(this); 
-        var id = love.attr("rel"); //对应id 
-        love.fadeOut(300); //渐隐效果 
-        $.ajax({ 
-            type:"GET", 
-            url:"json/love_activity.php", 
-            data:"activity="+id, 
-            cache:false, //不缓存此页面 
-            success:function(data){ 
-                love.html(data); 
-                love.fadeIn(300); //渐显效果 
-            } 
-        }); 
-        return false; 
-        */
-    $("#love").click(function(){ 
 
+    //点击“我也喜欢”按钮
+    $("#love").click(function(){ 
         $.ajax({ 
             type:"POST",
             url:"php/love_activity.php", 
@@ -65,24 +71,25 @@ $(document).ready(function() {
             success:function(data){
                 like_num = data.love_count;
                 is_love = data.is_love;
-
-                $('#love-num').empty();
-                $('#love-num').append(like_num);
+                loveNum.empty();
+                loveNum.html(like_num);
 
                 //判定是否喜欢了活动
-                if(is_love == 0)
-                    $('#love').attr('class','collect-btn');
-                else if(is_love == 1)
-                    $('#love').attr('class','');
-                else
+                if(is_love === 0){
+                    loveBtn.attr('class','collect-btn');
+                    loveBtn.html($wantlove);
+                }else if(is_love === 1){
+                    loveBtn.attr('class','already-btn');
+                    loveBtn.html($alreadylove);
+                }else{
                     console.log("服务器错误,请重新加载页面");
-
+                }
             } 
         });        
     }); 
 
+    //点击“我要参与”按钮
     $("#join").click(function(){ 
-
         $.ajax({ 
             type:"POST",
             url:"php/join_activity.php", 
@@ -91,18 +98,21 @@ $(document).ready(function() {
             success:function(data){
                 join_num = data.join_count;
                 is_join = data.is_join;
-
-                $('#join-num').empty();
-                $('#join-num').append(join_num);
-
+                joinNum.empty();
+                joinNum.append(join_num);
 
                 //判定是否参与了活动
-                if(is_join == 0)
-                    $('#join').attr('class','collect-btn');
-                else if(is_love == 1)
-                    $('#join').attr('class','');
-                else
+                if(is_join === 0){
+                    joinBtn.attr('class','collect-btn');
+                    joinBtn.html($wantjoin);
+                }
+                else if(is_join === 1){
+                    joinBtn.attr('class','already-btn');
+                    joinBtn.html($alreadyjoin);
+                }
+                else{
                     console.log("服务器错误,请重新加载页面");
+                }
 
             } 
         });        
