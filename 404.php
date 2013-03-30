99b3352d1d83fa91c0b62f7f88ef309f33c05a9b
Li WenChao <lee929@live.cn>
2013-03-30 15:54:55 +0800

    各种倒数

--- 404.php
+++ 404.php
@@ -4,14 +4,30 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>宅不起 | 页面不存在</title>
     <link href="css/404.css" type="text/css" rel="stylesheet" />
-</head>
-<body>
+    <script language="javascript">
+    var cdtime = 4;
+    var intervalid = setInterval("onload()",1000);
+    function onload()
+    {
+        var cdbox = document.getElementById("cdbox");
+        if(cdtime === 0){
+            //跳转到指定页面代码
+            window.location.href = "index.php";
+            clearInterval(intervalid);
+        }
+        cdbox.innerText = cdtime+'秒...';
+        cdtime--;
+    }
+    </script>
+    </head>
+<body onload="onload()">
     <?php require_once("top-nav.php");?>
     <div id="container">
         <div id="container-clear-fix">
             <div class="textbox">
                 <h1>404! 您想访问的页面<br />不不不...不存在</h1>
-                <a href="index.php">返回首页 Zhaibuqi.com</a>         
+                <span id="cdbox"></span>
+                <a href="index.php">返回首页 Zhaibuqi.com</a>
             </div>
             <img src="images/404.jpg" alt="页面不存在" width="200" height="227" />
         </div>
